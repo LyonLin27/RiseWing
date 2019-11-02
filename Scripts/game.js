dd54c75ee58b94cc9a05e731f3b1e80b541be13d
lionlin27 <linh8@rpi.edu>
2019-11-02 15:16:47 -0400

    full screen attempt

--- Scripts/game.js
+++ Scripts/game.js
@@ -2,7 +2,7 @@ var game;
 window.onload = function(){
     var config = {
         width: 1125,
-        height: 2000,//2436,
+        height: 2436,
         backgroundColor: 0xFFFFFF,
         scene: [titleScene, loadScene1, playScene1, loadScene2, playScene2, loadScene3, playScene3, endingBad, endingGood, testScene],
         pixelArt: true,
@@ -13,6 +13,10 @@ window.onload = function(){
                 gravity: { y: 200 }
             }
         },
+        scale: {
+            mode: Phaser.Scale.FIT,
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+        }
         // plugins: {
         //     scene: [{
         //         key: 'rexUI',
